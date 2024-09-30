ecdd1b8695bc3f4feaa5b905b05851b193b5f0d0
Simon Oehrl <simon.oehrl@rwth-aachen.de>
2024-09-30 19:09:11 +0200

    feat: expose measurment functionality

--- src/dtrack.cpp
+++ src/dtrack.cpp
@@ -25,4 +25,43 @@ bool sdk_is_valid(void *sdk) { return ((DTrackSDK *)sdk)->isValid(); }
 bool sdk_is_data_interface_valid(void *sdk) {
   return ((DTrackSDK *)sdk)->isDataInterfaceValid();
 }
+
+unsigned short sdk_get_data_port(void *sdk) {
+  return ((DTrackSDK *)sdk)->getDataPort();
+}
+
+bool sdk_is_command_interface_valid(void *sdk) {
+  return ((DTrackSDK *)sdk)->isCommandInterfaceValid();
+}
+
+bool sdk_is_command_interface_full_access(void *sdk) {
+  return ((DTrackSDK *)sdk)->isCommandInterfaceFullAccess();
+}
+
+int sdk_get_remote_system_type(void *sdk) {
+  return ((DTrackSDK *)sdk)->getRemoteSystemType();
+}
+
+bool sdk_set_data_timeout_us(void *sdk, int timeout_us) {
+  return ((DTrackSDK *)sdk)->setDataTimeoutUS(timeout_us);
+}
+
+bool sdk_set_command_timeout_us(void *sdk, int timeout_us) {
+  return ((DTrackSDK *)sdk)->setCommandTimeoutUS(timeout_us);
+}
+
+bool sdk_set_controller_timeout_us(void *sdk, int timeout_us) {
+  return ((DTrackSDK *)sdk)->setControllerTimeoutUS(timeout_us);
+}
+
+bool sdk_receive(void *sdk) { return ((DTrackSDK *)sdk)->receive(); }
+
+bool sdk_start_measurement(void *sdk) {
+  return ((DTrackSDK *)sdk)->startMeasurement();
+}
+
+bool sdk_stop_measurement(void *sdk) {
+  return ((DTrackSDK *)sdk)->stopMeasurement();
+}
+
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::ffi::{c_char, c_void};
+use std::ffi::{c_char, c_int, c_ushort, c_void};
 
 extern "C" {
     fn sdk_new_with_connection(connection: *const c_char) -> *mut c_void;
@@ -6,8 +6,26 @@ extern "C" {
 
     fn sdk_is_valid(sdk: *mut c_void) -> bool;
     fn sdk_is_data_interface_valid(sdk: *mut c_void) -> bool;
+    fn sdk_get_data_port(sdk: *mut c_void) -> c_ushort;
+    fn sdk_is_command_interface_valid(sdk: *mut c_void) -> bool;
+    fn sdk_is_command_interface_full_access(sdk: *mut c_void) -> bool;
+    fn sdk_get_remote_system_type(sdk: *mut c_void) -> c_int;
+    fn sdk_set_data_timeout_us(sdk: *mut c_void, timeout_us: c_int);
+    fn sdk_set_command_timeout_us(sdk: *mut c_void, timeout_us: c_int);
+    fn sdk_set_controller_timeout_us(sdk: *mut c_void, timeout_us: c_int);
+    fn sdk_receive(sdk: *mut c_void) -> bool;
+    fn sdk_start_measurement(sdk: *mut c_void) -> bool;
+    fn sdk_stop_measurement(sdk: *mut c_void) -> bool;
 }
 
+#[derive(Debug)]
+pub enum RemoteSystemType {
+    Unknown,
+    DTrack,
+    DTrack2,
+}
+
+#[derive(Debug)]
 pub struct DTrack {
     sdk: *mut c_void,
 }
@@ -36,5 +54,38 @@ impl DTrack {
 
     pub fn is_data_interface_valid(&self) -> bool {
         unsafe { sdk_is_data_interface_valid(self.sdk) }
+    }
+
+    pub fn data_port(&self) -> u16 {
+        unsafe { sdk_get_data_port(self.sdk) }
+    }
+
+    pub fn is_command_interface_valid(&self) -> bool {
+        unsafe { sdk_is_command_interface_valid(self.sdk) }
+    }
+
+    pub fn is_command_interface_full_access(&self) -> bool {
+        unsafe { sdk_is_command_interface_full_access(self.sdk) }
+    }
+
+    pub fn remote_system_type(&self) -> RemoteSystemType {
+        match unsafe { sdk_get_remote_system_type(self.sdk) } {
+            0 => RemoteSystemType::Unknown,
+            1 => RemoteSystemType::DTrack,
+            2 => RemoteSystemType::DTrack2,
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn receive(&self) -> bool {
+        unsafe { sdk_receive(self.sdk) }
+    }
+
+    pub fn start_measurement(&self) -> bool {
+        unsafe { sdk_start_measurement(self.sdk) }
+    }
+
+    pub fn stop_measurement(&self) -> bool {
+        unsafe { sdk_stop_measurement(self.sdk) }
     }
 }
